f8774ea0ebd3b1a50ee42dda818b9c70144b2be8
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-06-12 21:51:09 +0200

    considere the restart_game and create_pin methods into a different module into the main rust file

--- rust-tic-tac-toe/src/main.rs
+++ rust-tic-tac-toe/src/main.rs
@@ -2,9 +2,9 @@ extern crate piston_window;
 extern crate sprite;
 extern crate uuid;
 
-use uuid::Uuid;
-
 use std::rc::Rc;
+
+use uuid::Uuid;
 
 use piston_window::{
     PistonWindow,
@@ -16,55 +16,14 @@ use piston_window::{
     ReleaseEvent,
     Button,
     MouseButton,
-    ImageSize,
     MouseCursorEvent,
 };
 
 use sprite::{
-    Sprite,
     Scene,
 };
 
-/// Displays a new pin on the table,
-/// the pin sprite is created from the given texture
-fn create_pin<T: ImageSize>(
-    scene: &mut Scene<T>,
-    uuids: &mut Vec<Uuid>,
-    texture: &Rc<T>,
-    cursor_position_x: &f64,
-    cursor_position_y: &f64,
-)
-{
-    let mut sprite = Sprite::from_texture(texture.clone());
-
-    /* cast into integers for euclidean division */
-    let pin_position_x: u32 = (*cursor_position_x as u32 / 126) * 126 + 66;
-    let pin_position_y: u32 = (*cursor_position_y as u32 / 114) * 114 + 57;
-
-    sprite.set_position(
-        pin_position_x as f64,
-        pin_position_y as f64,
-    );
-
-    let uuid: Uuid = scene.add_child(sprite);
-    uuids.push(uuid);
-}
-
-/// Removes all children from the scene and empty the uuids vector
-fn restart_game<T: ImageSize>(
-    scene: &mut Scene<T>,
-    uuids: &mut Vec<Uuid>,
-)
-{
-    /* first dereference uuids to get the vector from its mutable reference;
-       do not borrow the uuids vector by prepending a & because of uuids.clear();
-       get a &Uuid, so we dereference it when call remove_child() */
-    for uuid in &*uuids {
-        scene.remove_child(*uuid);
-    }
-
-    uuids.clear();
-}
+mod utils;
 
 fn main() {
 
@@ -128,7 +87,7 @@ fn main() {
 
         if let Some(Button::Mouse(MouseButton::Left)) = event.release_args() {
 
-            create_pin(
+            utils::create_pin(
                 &mut scene,
                 &mut uuids,
                 &black,
@@ -139,7 +98,7 @@ fn main() {
 
         if let Some(Button::Mouse(MouseButton::Right)) = event.release_args() {
 
-            restart_game(
+            utils::restart_game(
                 &mut scene,
                 &mut uuids,
             );
